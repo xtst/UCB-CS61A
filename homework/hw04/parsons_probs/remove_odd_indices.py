9b67e4c903777e36690e88633bf44e57149eb3f8
xtst <xintongsanto@yeah.net>
2022-06-16 22:57:16 +0800

    hw04 update

--- homework/hw04/parsons_probs/remove_odd_indices.py
+++ homework/hw04/parsons_probs/remove_odd_indices.py
@@ -1,5 +1,8 @@
+from curses.ascii import FF
+
+
 def remove_odd_indices(lst, odd):
-    """ 
+    """
     Remove elements of lst that have odd indices.
     >>> s = [1, 2, 3, 4]
     >>> t = remove_odd_indices(s, True)
@@ -12,5 +15,19 @@ def remove_odd_indices(lst, odd):
     >>> m
     [6, 8]
     """
-    for i in range(0,len(lst)):
-        if (i%2==0)
+
+    def letin(index):
+        if index % 2 == 0:
+            if odd:
+                return True
+            return False
+        if index % 2 == 1:
+            if not odd:
+                return True
+            return False
+
+    ans = []
+    for i in range(0, len(lst)):
+        if letin(i):
+            ans.append(lst[i])
+    return ans
