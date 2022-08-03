8c633714c68261d99e32041a9f69849c8f9d0fa2
otnasxin@163.com <xintongsanto@yeah.net>
2022-08-03 15:19:52 +0800

    hw6 part done

--- homework/hw06/hw06.scm
+++ homework/hw06/hw06.scm
@@ -18,6 +18,19 @@
     (if (null? lst2) lst1 (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2))))
 )))
 
-(define (my-filter func lst) 'YOUR-CODE-HERE)
+(define (my-filter func lst) 
+    (if (null? lst) lst
+        (if(func (car lst)) 
+            (cons (car lst) (my-filter func (cdr lst)))
+            (my-filter func (cdr lst))
+        )
+    )
+)
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+    (if (null? lst) lst
+        (
+            cons (car lst) (no-repeats (filter (lambda (x) (not (= x (car lst)))) (cdr lst)))
+        )
+    )
+)
